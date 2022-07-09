ec0bec56bfe97900d991ce481021e7cab2d6f993
ir1s0047 <214749466@qq.com>
2022-07-09 21:25:29 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Order from '@/components/Order.vue';
+import Sort from '@/components/Sort.vue';
+import Index from '@/components/Index.vue';
+import My from '@/components/My.vue';
 
-Vue.config.productionTip = false
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
+const routes = [
+  { path: '/', redirect: '/Index' },
+  { path: '/Order', component: Order },
+  { path: '/Sort', component: Sort },
+  { path: '/Index', component: Index },
+  { path: '/My', component: My },
+];
+
+const router = new VueRouter({
+  routes,
+});
+Vue.config.productionTip = false;
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  router,
+}).$mount('#app');
